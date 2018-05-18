15ffa6ba7a059e77709b25f744d161482a63e279
Dzmitryi Drozd <dzmitryi_drozd@epam.com>
2018-05-18 17:22:54 +0300

    Added base structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { CourseComponent } from './pages/courses/course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    LoginPageComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
